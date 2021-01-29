6c531151d7f91f5aab48dc36ea44f11b1c4ec4a8
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-01-29 20:18:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     """ Here's all the messages will be handled and processed by the program """
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
